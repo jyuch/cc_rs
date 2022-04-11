c66f31ff457e5eb703c5960204e71f7a38cd1f6e
jyuch <jyuch@users.noreply.github.com>
2022-04-11 20:22:25 +0900

    加減算のできるコンパイラの作成

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,58 @@ fn main() {
         process::exit(1);
     }
 
-    let n: i32 = args[1].parse().unwrap();
+    let (n, mut remain) = strton(&args[1]);
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
     println!("  mov rax, {}", n);
+
+    loop {
+        if remain.is_empty() {
+            break;
+        }
+
+        if remain[0..1].eq("+") {
+            let r = &remain[1..remain.len()];
+            let (n, r) = strton(r);
+            println!("  add rax, {}", n);
+            remain = r;
+            continue;
+        }
+
+        if remain[0..1].eq("-") {
+            let r = &remain[1..remain.len()];
+            let (n, r) = strton(r);
+            println!("  sub rax, {}", n);
+            remain = r;
+            continue;
+        }
+
+        eprintln!("予期しない文字です:{}", &remain[0..remain.len()]);
+        process::exit(1);
+    }
+
     println!("  ret");
     process::exit(0);
+}
+
+fn strton(value: &str) -> (i32, String) {
+    let position = value
+        .find(|it: char| !it.is_numeric())
+        .unwrap_or(value.len());
+
+    let n: i32 = value[0..position].parse().unwrap();
+    (n, value[position..].to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn strton_test() {
+        let (n, remain) = strton("123+");
+        assert_eq!(n, 123);
+        assert_eq!(remain, "+");
+    }
 }
